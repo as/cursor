e37f29fc1d09c55b097c80fd5495e8675bbbf03f
as <as.utf8@gmail.com>
2017-07-23 22:16:40 -0700

    read/write plan9

--- cursor_windows.go
+++ cursor_windows.go
@@ -1,15 +1,14 @@
 package cursor
 
-import(
-	"syscall"
+import (
 	"image"
+	"syscall"
 )
 
-var(
-	user32 = syscall.MustLoadDLL("user32.dll")
+var (
+	user32       = syscall.MustLoadDLL("user32.dll")
 	setCursorPos = user32.MustFindProc("SetCursorPos")
 )
-
 
 func moveTo(p image.Point) bool {
 	r, _, _ := setCursorPos.Call(uintptr(p.X), uintptr(p.Y))
