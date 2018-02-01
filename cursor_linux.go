9f135326806535b4e922add754b82aaf6da0fef6
as <as.utf8@gmail.com>
2018-01-31 18:17:19 -0800

    cursor: linux: implement

--- cursor_linux.go
+++ cursor_linux.go
@@ -1,8 +1,20 @@
 package cursor
 
 import (
+	"fmt"
 	"image"
+	"os/exec"
 )
+
+func widSelf() string {
+	out, _ := exec.Command("xdotool", "getactivewindow").Output()
+	return string(out)
+}
+
+func MoveRelativeTo(p image.Point) {
+	id := widSelf()
+	exec.Command("xdotool", "mousemove", "-w", id, fmt.Sprint(p.X), fmt.Sprint(p.Y))
+}
 
 func MoveTo(p image.Point) bool {
 	return moveTo(p)
